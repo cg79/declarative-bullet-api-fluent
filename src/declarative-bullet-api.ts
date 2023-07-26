3ef09caebd58bb3354e68ce458e7172fff9ba686
Claudiu Gombos <claudiu.gombos@thinslices.com>
2023-07-26 11:47:10 +0300

    added declarative bullet api

--- src/declarative-bullet-api.ts
+++ src/declarative-bullet-api.ts
@@ -1,72 +1,64 @@
 import { CustomHttpResponse } from './CustomHttpResponse';
 import { BulletAuthentication } from './facade';
-import FluentBulletBase from './fluent/fluent-bullet-base';
 import BulletHttpRequestLibrary from './BulletHttpRequestLibrary';
 import WrapperFlow, { IWrapperFlow } from './fluent/wrapper-flow';
 import { FlowFunctionType } from './fluent/bullet-flow';
+import { FluentBulletBase } from './fluent/fluent-bullet-base';
 
-class DeclarativeBulletApi extends FluentBulletBase {
-  private bulletAuthentication: BulletAuthentication;
-
-  constructor(bulletAuthentication: BulletAuthentication) {
-    super();
+ const DeclarativeBulletApi = function(bulletAuthentication: BulletAuthentication){
     this.bulletAuthentication = bulletAuthentication;
-    // this.bulletAuthentication = {
-    //   ...bulletAuthentication,
-    //   serverUrl: `${bulletAuthentication.serverUrl}${BULLET_ROUTE}`,
-    // };
-  }
 
-  protected flowList = [];
-  flow(flowBuilder: FlowFunctionType) {
-    const flowWrap =  WrapperFlow().load(flowBuilder);
+    return {
+      ...FluentBulletBase(),
+      flow: function(flowBuilder: FlowFunctionType) {
+        const flowWrap =  WrapperFlow().load(flowBuilder);
+    
+        // this.flowInstance = flowWrap;
+        this.flowList.push(flowWrap);
+    
+        return this;
+      },
 
-    // this.flowInstance = flowWrap;
-    this.flowList.push(flowWrap);
-
-    return this;
-  }
-
-  asJson() {
-    const response = {
-      ...this.asJsonBase(),
-    };
-
-    const flowListLength = this.flowList.length;
-    if (flowListLength) {
-      if(flowListLength === 1){
-        response.flow = this.flowList[0].asJson();
-      } else {
-        const jsFlows = [];
-        this.flowList.forEach((el: IWrapperFlow)=> jsFlows.push(el.asJson()));
-        response.flow = jsFlows;
+      asJson: function() {
+        const response = {
+          ...this.asJsonBase(),
+        };
+    
+        const flowListLength = this.flowList.length;
+        if (flowListLength) {
+          if(flowListLength === 1){
+            response.flow = this.flowList[0].asJson();
+          } else {
+            const jsFlows = [];
+            this.flowList.forEach((el: IWrapperFlow)=> jsFlows.push(el.asJson()));
+            response.flow = jsFlows;
+          }
+        }
+    
+        return response;
+      },
+      execute: function(handlers?: {
+        beforeSendingRequest?: Function;
+      }): Promise<CustomHttpResponse> {
+        // debugger;
+        const request = this.asJson();
+    
+        if (handlers && handlers.beforeSendingRequest) {
+          handlers.beforeSendingRequest(request);
+        }
+        const bulletHttpInstance = new BulletHttpRequestLibrary(
+          this.bulletAuthentication
+        );
+    
+        if (!this.storageInstance) {
+          return bulletHttpInstance.sendBulletApiRequest(request);
+        }
+        return bulletHttpInstance.sendBulletFilesApiRequest(
+          request,
+          this.storageInstance.files
+        );
       }
     }
-
-    return response;
-  }
-
-  execute(handlers?: {
-    beforeSendingRequest?: Function;
-  }): Promise<CustomHttpResponse> {
-    // debugger;
-    const request = this.asJson();
-
-    if (handlers && handlers.beforeSendingRequest) {
-      handlers.beforeSendingRequest(request);
-    }
-    const bulletHttpInstance = new BulletHttpRequestLibrary(
-      this.bulletAuthentication
-    );
-
-    if (!this.storageInstance) {
-      return bulletHttpInstance.sendBulletApiRequest(request);
-    }
-    return bulletHttpInstance.sendBulletFilesApiRequest(
-      request,
-      this.storageInstance.files
-    );
-  }
 }
 
 export default DeclarativeBulletApi;
